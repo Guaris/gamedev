22935818fbf89ded0bca4dd015c6893622702bf0
Angel <aguarisma@linode.com>
2018-12-07 15:42:15 -0500

    Added Double Jump & Wall Jump

--- testmove/KinematicBody2D.gd
+++ testmove/KinematicBody2D.gd
@@ -5,9 +5,24 @@ const GRAVITY = 20
 const ACCELERATION = 50
 const MAX_SPEED = 200
 const JUMP_HEIGHT = -550
+const MAX_JUMPS = 2
+const JUMPS = 0
 
 var motion = Vector2()
 
+func _input(event):
+	if JUMPS < MAX_JUMPS:
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+			JUMPS += 1
+	if is_on_floor():
+		JUMPS = 0
+	if motion.y > 0 and is_on_wall():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+			JUMPS += 1
+	if is_on_floor():
+		JUMPS = 0
 
 func _physics_process(delta):
 	motion.y += GRAVITY
@@ -27,6 +42,7 @@ func _physics_process(delta):
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+			JUMPS += 1
 		if friction == true:
 			motion.x = lerp(motion.x, 0, 0.2)
 	else:
@@ -38,4 +54,3 @@ func _physics_process(delta):
 			motion.x = lerp(motion.x, 0, 0.05)
 	motion = move_and_slide(motion, UP)
 	pass
-	
